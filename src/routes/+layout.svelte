7d503461aa844076637ba416338154adc15a7db8
AxelDevara <axeldevara@gmail.com>
2022-10-31 12:29:39 +0700

    base UI

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+  import "../app.postcss";
+</script>
+
+<main class=" h-screen flex flex-col overflow-x-hidden bg-gradient-to-b from-green-300 to-slate-100">
+  <nav class=" bg-white mx-20 mt-6 grid justify-center items-center grid-cols-8 rounded-md  sticky top-0 z-10 h-14">
+    <p class="col-span-5 pl-10 text-lg font-bold">Farm3</p>
+    <p>Pools</p>
+    <p>About</p>
+    <button class="bg-orange-500 h-full rounded-r-md text-white">Connect Wallet</button>
+  </nav>
+  <slot />
+</main>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    const arr = [{title:"Wheat"}, {title:"Grape"}]
+</script>
+
+<div class="w-full h-full grid grid-cols-4 gap-3 mx-20 mt-6">
+    {#each arr as item}
+    <div class="bg-white bg-opacity-25 rounded-md p-6 h-20">{item.title}</div>
+{/each}
+</div>
