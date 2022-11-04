019a3f50ad5fad50ab3a33c4933e96f3cb4956d9
AxelDevara <axeldevara@gmail.com>
2022-11-04 10:06:32 +0700

    changed UI

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,13 @@
   import "../app.postcss";
 </script>
 
-<main class=" h-screen flex flex-col overflow-x-hidden bg-gradient-to-b from-green-300 to-slate-100">
-  <nav class=" bg-white mx-20 mt-6 grid justify-center items-center grid-cols-8 rounded-md  sticky top-0 z-10 h-14">
-    <p class="col-span-5 pl-10 text-lg font-bold">Farm3</p>
-    <p>Pools</p>
-    <p>About</p>
-    <button class="bg-orange-500 h-full rounded-r-md text-white">Connect Wallet</button>
+<main class=" h-screen flex flex-col overflow-x-hidden bg-yellow-400 text-white">
+  <nav class="mx-10 mt-6 flex justify-between items-center rounded-md  sticky top-0 z-10 h-14">
+    <p class="col-span-5 text-3xl font-bold">HoneyLemon</p>
+    <button class="bg-white px-10 h-full rounded-full text-yellow-400">Connect Wallet</button>
   </nav>
-  <slot />
+  <div class="mx-10 mt-8 text-neutral-500">
+    <slot />
+  </div>
 </main>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,21 @@
 <script>
-    const arr = [{title:"Wheat"}, {title:"Grape"}]
+    const arr = ["Asset", "Price", "Value", "Expiry Date"];
 </script>
 
-<div class="w-full h-full grid grid-cols-4 gap-3 mx-20 mt-6">
-    {#each arr as item}
-    <div class="bg-white bg-opacity-25 rounded-md p-6 h-20">{item.title}</div>
-{/each}
+<div class="w-full h-10 grid grid-cols-6">
+    <div class="col-span-2"></div>
+    <div class="col-span-2 bg-white rounded-full p-1">
+        <div class="grid grid-cols-2 bg-neutral-100 h-full w-full rounded-full">
+            <button class="bg-yellow-400 rounded-full text-white">Investor</button>
+            <button>Farmer</button>
+        </div>
+    </div>
+    <div class="col-span-2"></div>
+</div>
+<div class="w-full bg-white rounded-xl mt-3 ">
+    <table class="w-full h-full border-b-2 border-neutral-400">
+        {#each arr as item}
+        <th class="bg-white bg-opacity-25 rounded-md p-6">{item}</th>
+    {/each}
+    </table>
 </div>
