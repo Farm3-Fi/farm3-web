83dec6d5b37acf7760c487cbb29c8eece974564a
AxelDevara <axeldevara@gmail.com>
2022-11-18 21:48:30 +0700

    fixed ethers

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,13 +3,14 @@ import "../app.postcss";
 import {ethers} from "ethers";
 import {wallet, provider} from "$lib/web3Store";
 
+
 const connectWallet = async () => {
   if (typeof window.ethereum !== 'undefined') {
   $provider = new ethers.providers.Web3Provider(window.ethereum)
   await $provider.send("eth_requestAccounts", []);
   $wallet = await $provider.getSigner().getAddress();
   const chainId = await $provider.send( 'eth_chainId', []);
-  const polygonChainID = '0x1';
+  const polygonChainID = '0x137';
   console.log($wallet)
   if(chainId != polygonChainID) {
     await $provider.send(
@@ -28,21 +29,23 @@ const connectWallet = async () => {
   <nav class="mx-10 mt-6 flex justify-between items-center rounded-md  sticky top-0 z-10 h-14">
     <p class="col-span-5 text-3xl font-bold text-neutral-800">HoneyLemon</p>
     <div class="h-full">
-      <button class="border-4 border-white px-10 h-full font-semibold rounded-full text-white">+Create Future</button>
+      
       <button on:click={connectWallet} class="bg-white px-10 h-full font-semibold rounded-full text-yellow-300">{$wallet != null ? $wallet.substring(0,8) + "..." : "Connect Wallet"}</button>
     </div>
   </nav>
   <div class="mx-10 z-10 mt-8 text-neutral-500">
     <slot />
   </div>
+  {#if $provider}
   <footer class="fixed bottom-0 p-4 w-full bg-neutral-800">
-    {#if $provider}
+    
     {#await $provider.getBlockNumber()}
     <p>loading block ....</p>
     {:then block}
       <p>Block {block}</p>      
     {/await}
-    {/if}
+    
   </footer>
+  {/if}
 </main>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,62 @@
 <script>
-    import TransactionModal from "$lib/modals/transactionModal.svelte";
-    let openModal = false;
-    const arr = ["Futures", "Price", "Value", "Expiry Date"];
-    const dummy = [{
-        asset: "WHEAT", price:"500", value:"20000", expiryDate: Date.now()
-    }, {
-        asset: "WHEAT", price:"500", value:"20000", expiryDate: Date.now()
-    }, {
-        asset: "WHEAT", price:"500", value:"20000", expiryDate: Date.now()
-    }]
+  import TransactionModal from "$lib/modals/transactionModal.svelte";
+  import futuresABI from "$lib/abis/futuresABI.json";
+  import {provider, wallet} from "$lib/web3Store";
+  import {ethers} from "ethers";
+
+
+  let openModal = false;
+  const arr = ["Futures", "Price", "Value", "Expiry Date"];
+  const list = [
+    "0xFdbEfDa8d1aF710cee0E6f76093D8A300B5EE1a7",
+    "0xd73bc8aff52df9bb93b3d52ba463535d8ff55062"
+  ];
+
+  const getData = async (contract) => {
+    const future = new ethers.Contract(contract, futuresABI, $provider);
+    const name = await future.symbol();
+    const supply = await future.totalSupply();
+    const baseprice = await future.BASEPRICE();
+    const expiryDate = await future.expiryDate();
+  
+    return {
+      name: name,
+      supply: supply,
+      baseprice: baseprice,
+      expiry: expiryDate
+    };
+  }
 </script>
-<TransactionModal bind:isOpen={openModal}></TransactionModal>
+
+<TransactionModal bind:isOpen={openModal} />
 <div class="w-full max-w-5xl mx-auto bg-neutral-800 rounded-xl mt-3 text-white">
-    <div class="w-full h-full grid grid-cols-4">
-        {#each arr as item}
-        <h1 class="rounded-md px-6 py-4">{item}</h1>
-        {/each}
-    </div>
+  <div class="w-full h-full grid grid-cols-4">
+    {#each arr as item}
+      <h1 class="rounded-md px-6 py-4">{item}</h1>
+    {/each}
+  </div>
 </div>
 
-<div class="w-full max-w-5xl mx-auto bg-white rounded-xl divide-y mt-2 text-black">
-    {#each dummy as item}
-    <button on:click={()=> openModal = !openModal} class="text-left w-full h-full grid grid-cols-4">
-        <p class="px-6 py-4 font-semibold">{item.asset}</p>
-        <p class="px-6 py-4">{item.price}</p>
-        <p class="px-6 py-4">{item.value}</p>
-        <p class="px-6 py-4">{item.expiryDate}</p>
+{#if $wallet}
+<div
+  class="w-full max-w-5xl mx-auto bg-white rounded-xl divide-y mt-2 text-black"
+>
+  {#each list as item}
+    <button
+      on:click={() => (openModal = !openModal)}
+      class="text-left w-full h-full grid grid-cols-4"
+    >
+     {#await getData(item)}
+        <p>Loading...</p>
+     {:then data} 
+     <p class="px-6 py-4 font-semibold">{data.name}</p>
+     <p class="px-6 py-4">{data.baseprice}</p>
+     <p class="px-6 py-4">{data.supply * data.baseprice}</p>
+     <p class="px-6 py-4">{data.expiry}</p>
+     {/await}
     </button>
-    {/each}
+  {/each}
 </div>
+{:else}
+<p class="w-full text-center mt-4 text-black">Please connect Metamask</p>
+{/if}
